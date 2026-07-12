491471be7261d45ac16ce3e099c26132ef7cc084
Priya Kourav <priyakourav55@gmail.com>
2026-07-12 17:17:48 +0530

    update education layout

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -9,12 +9,12 @@ export const educations = [
     id: 2,
     title: "Higher Secondary Education (Class XII)",
     duration: "2022 - 2023",
-    institution: "Vivek Jagrati Higher Secondary School (MP Board) • 72.2%",
+    institution: "Vivek Jagrati Vidhya Mandir Higher Secondary School • 72.2%",
   },
   {
     id: 3,
     title: "Secondary Education (Class X)",
     duration: "2020 - 2021",
-    institution: "Vivek Jagrati Higher Secondary School (MP Board) • 91.8%",
+    institution: "Vivek Jagrati Vidhya Mandir Higher Secondary School • 91.8%",
   }
 ]
