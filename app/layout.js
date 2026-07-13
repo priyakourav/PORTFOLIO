51566cd4132d2798e8b3bceca8d9f4fc65070ca3
Priya Kourav <priyakourav55@gmail.com>
2026-07-13 15:38:40 +0530

    update route.js

--- app/layout.js
+++ app/layout.js
@@ -27,3 +27,20 @@ export const metadata = {
     "Next.js",
   ],
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className} suppressHydrationWarning>
+        <ToastContainer />
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+          <Navbar />
+          {children}
+          <ScrollToTop />
+        </main>
+        <Footer />
+      </body>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+    </html>
+  );
+}
